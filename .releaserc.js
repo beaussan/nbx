8e1a7f840052ab34b24e0e3af0fc2398ed6d6b2d
Nicolas Beaussart <nic.beaussart@gmail.com>
2019-11-20 15:53:48 +0100

    :wrench: change release manager config

--- .releaserc.js
+++ .releaserc.js
@@ -12,42 +12,15 @@
 
 module.exports = {
     plugins: [
-      [
-        'semantic-release-gitmoji', {
-            /*
-          releaseRules: {
-            major: [ ':boom:' ],
-            minor: [ ':sparkles:' ],
-            patch: [
-              ':bug:',
-              ':ambulance:',
-              ':lock:'
-            ]
-          },
-          releaseNotes: {
-            //template,
-            // partials: { commitTemplate },
-            helpers: {
-              datetime: function (format = 'UTC:yyyy-mm-dd') {
-                return dateFormat(new Date(), format)
-              }
-            },
-            issueResolution: {
-              template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-              baseUrl: 'https://github.com',
-              source: 'github.com'
-            }
-          }
-          */
-        }
-      ],
-      // '@semantic-release/github',
-      // '@semantic-release/npm'
+      'semantic-release-gitmoji',
+      '@semantic-release/github',
+      '@semantic-release/npm',
       ["@semantic-release/changelog", {
         "changelogFile": "CHANGELOG.md",
       }],
       ["@semantic-release/git", {
         "assets": ["CHANGELOG.md", "package.json"],
-      }],
+        "message": ":bookmark: ${nextRelease.version}\n\n${nextRelease.notes}"
+      }]
     ]
   }
